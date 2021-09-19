7ebf6323969a2b4aa1bd331613f4dda50a3b4730
kadum komut <kadumeliza@gmail.com>
2021-09-19 21:19:46 +0530

    first commiet

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ const profileRoute = require('./routes/profileRoute')
 const app = express();
 
 // middle ware
-app.use(cors({origin:'https://recode-snippet.web.app/'}))
+app.use(cors({origin:process.env.CORS_ORIGIN_URL}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 // using routes
 app.use('/profile',profileRoute)
 app.use('/snippet',snippetRoute)
-
 
 // main route
 app.route('/')
